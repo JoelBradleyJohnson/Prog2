94fbbaeeb484ee33b88b84978cb67b7e3d54f3a6
JoelBradleyJohnson <216280@student.cofo.edu>
2020-01-17 20:46:47 -0600

    Final Update

--- Circuit/src/ui/Main.java
+++ Circuit/src/ui/Main.java
@@ -11,6 +11,7 @@ import bp.ICircuit;
  * @author 216280 This class holds the interaction with the user.
  */
 public class Main {
+
 	/**
 	 * This constant holds the value of the user choice.
 	 */
@@ -33,51 +34,65 @@ public class Main {
 		Scanner keyboard = new Scanner(System.in);
 		ICircuit myCircuit = new Circuit();
 		int userChoice;
+		String userAnswer;
 
-		System.out.println("The purpose of this program is to calculate a variable based on the user's choice.");
+		System.out.println("Welcome to the Voltage, Amperage, and Resistance calculator. Prepair to be amazed.");
 
-		System.out.println("Would you like to calculate:\n" + "1) Voltage,\n" + "2) Amperage,\n" + "or 3) Resistance?");
+		do {
+			System.out.println("Would you like to calculate:\n"
+					+ "1) Voltage,\n"
+					+ "2) Amperage, or\n"
+					+ "3) Resistance?");
 
-		userChoice = keyboard.nextInt();
+			userChoice = keyboard.nextInt();
 
-		if (userChoice == USERCHOSEVOLTAGE) {
-			System.out.println("Please enter amperage:");
-			myCircuit.setAmperage(keyboard.nextDouble());
+			if (userChoice == USERCHOSEVOLTAGE) {
+				System.out.println("Please enter amperage:");
+				myCircuit.setAmperage(keyboard.nextDouble());
 
-			System.out.println("Please enter resistance:");
-			myCircuit.setResistance(keyboard.nextDouble());
+				System.out.println("Please enter resistance:");
+				myCircuit.setResistance(keyboard.nextDouble());
 
-			myCircuit.calculateVoltage();
+				myCircuit.calculateVoltage();
 
-			System.out.println("The voltage is " + myCircuit.getVoltage() + " volts.");
+				System.out.println("The voltage is " + myCircuit.getVoltage() + " volts.");
 
-		} else if (userChoice == USERCHOSEAMPERAGE) {
-			System.out.println("Please enter voltage:");
-			myCircuit.setVoltage(keyboard.nextDouble());
+			} else if (userChoice == USERCHOSEAMPERAGE) {
+				System.out.println("Please enter voltage:");
+				myCircuit.setVoltage(keyboard.nextDouble());
 
-			System.out.println("Please enter resistance:");
-			myCircuit.setResistance(keyboard.nextDouble());
+				System.out.println("Please enter resistance:");
+				myCircuit.setResistance(keyboard.nextDouble());
 
-			myCircuit.calculateAmperage();
+				myCircuit.calculateAmperage();
 
-			System.out.println("The amperage is " + myCircuit.getAmperage() + " amps.");
+				System.out.println("The amperage is " + myCircuit.getAmperage() + " amps.");
 
-		} else if (userChoice == USERCHOSERESISTANCE) {
+			} else if (userChoice == USERCHOSERESISTANCE) {
+				System.out.println("Please enter voltage:");
+				myCircuit.setVoltage(keyboard.nextDouble());
 
-			System.out.println("Please enter voltage:");
-			myCircuit.setVoltage(keyboard.nextDouble());
+				System.out.println("Please enter amperage:");
+				myCircuit.setAmperage(keyboard.nextDouble());
 
-			System.out.println("Please enter amperage:");
-			myCircuit.setAmperage(keyboard.nextDouble());
+				myCircuit.calculateResistance();
 
-			myCircuit.calculateResistance();
+				System.out.println("The resistance is " + myCircuit.getResistance() + ".");
 
-			System.out.println("The resistance is " + myCircuit.getResistance() + ".");
+			} else {
+				System.out.println("That input was invalid.");
+			}
 
-		} else {
-			System.out.println("That input was invalid.");
-		}
+			// Burn line
+			keyboard.nextLine();
+			
+			System.out.println("Would you like to make another calculation?");
+			userAnswer = keyboard.nextLine();
 
+		} while (userAnswer.equals("yes") || userAnswer.equals("Yes"));
+
+		System.out.println("Have a great day!");
+		
 		keyboard.close();
 	}
 
