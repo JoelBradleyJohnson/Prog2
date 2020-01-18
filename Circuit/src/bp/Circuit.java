2c4eb2a4bd94d0efa6c75d44a79a1af0604034f8
JoelBradleyJohnson <216280@student.cofo.edu>
2020-01-17 20:35:37 -0600

    Update 1-17 8:35P

---
+++ Circuit/src/bp/Circuit.java
@@ -0,0 +1,73 @@
+/**
+ * This package contains business process logic.
+ */
+package bp;
+
+/**
+ * @author jjohnson This class holds the necessary stuff.
+ */
+public class Circuit implements ICircuit {
+
+	// *******Primitive Variables*******
+	/**
+	 * Primitive variable that holds voltage.
+	 */
+	private double voltage;
+	/**
+	 * Primitive variable that holds amperage.
+	 */
+	private double amperage;
+	/**
+	 * Primitive variable that holds resistance.
+	 */
+	private double resistance;
+
+	// *******Accessors********
+	@Override
+	public final double getVoltage() {
+		return voltage;
+	}
+
+	@Override
+	public final double getAmperage() {
+		return amperage;
+	}
+
+	@Override
+	public final double getResistance() {
+		return resistance;
+	}
+
+	// *******Mutators*******
+	@Override
+	public final void setVoltage(final double pVoltage) {
+		voltage = pVoltage;
+	}
+
+	@Override
+	public final void setAmperage(final double pAmperage) {
+		amperage = pAmperage;
+	}
+
+	@Override
+	public final void setResistance(final double pResistance) {
+		resistance = pResistance;
+	}
+
+	// *******Calculations*******
+	@Override
+	public final void calculateVoltage() {
+		voltage = amperage * resistance;
+	}
+
+	@Override
+	public final void calculateAmperage() {
+		amperage = voltage / resistance;
+	}
+
+	@Override
+	public final void calculateResistance() {
+		resistance = voltage / amperage;
+	}
+
+}
