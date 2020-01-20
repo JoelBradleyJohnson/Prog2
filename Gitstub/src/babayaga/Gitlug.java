2e5128666885bcc9b2a2e4e7a369faaa8bd59d15
216280 <216280@PLSLAB20751.hardworku.cofo.edu>
2020-01-20 11:16:51 -0600

    Getlug is stupid

---
+++ Gitstub/src/babayaga/Gitlug.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package babayaga;
+
+/**
+ * @author 216280
+ *
+ */
+public class Gitlug {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Getstub is stupid.");
+
+	}
+
+}
