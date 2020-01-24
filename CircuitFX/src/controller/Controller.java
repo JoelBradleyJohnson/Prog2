2e7ab89561980208fa1a74080c0769cd50780849
216280 <216280@PLSLAB20751.hardworku.cofo.edu>
2020-01-24 11:47:39 -0600

    Semi-Final

--- CircuitFX/src/controller/Controller.java
+++ CircuitFX/src/controller/Controller.java
@@ -4,15 +4,56 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.TextField;
+import model.Circuit;
 
 public class Controller {
 
 	@FXML
+	private TextField txtVoltage;
+
+	@FXML
+	private TextField txtResistance;
+
+	@FXML
+	private TextField txtAmperage;
+
+	@FXML
 	void handleClick(ActionEvent event) {
-		Alert myAlert = new Alert(AlertType.INFORMATION);
-		myAlert.setTitle("Christian");
-		myAlert.setContentText("Christian is a cookie boi with a lot of cookies.");
-		myAlert.showAndWait();
+		Circuit myCircuit = new Circuit();
+	
+		if (txtVoltage.getText().isBlank()) { //User must want to calculate voltage.
+			myCircuit.setAmperage(Double.parseDouble(txtAmperage.getText()));
+			myCircuit.setResistance(Double.parseDouble(txtResistance.getText()));
+			myCircuit.calculateVoltage();
+			Alert myAlert = new Alert(AlertType.INFORMATION);
+			myAlert.setTitle("Voltaje");
+			myAlert.setContentText(String.valueOf(myCircuit.getVoltage()));
+			myAlert.showAndWait();
+		}
+		
+		if (txtResistance.getText().isBlank()) { //User must want to calculate resistance.
+			myCircuit.setVoltage(Double.parseDouble(txtVoltage.getText()));
+			myCircuit.setAmperage(Double.parseDouble(txtAmperage.getText()));
+			myCircuit.calculateResistance();
+			txtResistance.setText(String.valueOf(myCircuit.getResistance()));
+		}
+		
+		if (txtAmperage.getText().isBlank()) { //User must want to calculate amperage.
+			myCircuit.setVoltage(Double.parseDouble(txtVoltage.getText()));
+			myCircuit.setResistance(Double.parseDouble(txtResistance.getText()));
+			myCircuit.calculateAmperage();
+			txtAmperage.setText(String.valueOf(myCircuit.getAmperage()));
+		}
+		
+	}
+	
+	@FXML
+	void handleClear(ActionEvent event) {
+		txtVoltage.clear();
+		txtAmperage.clear();
+		txtResistance.clear();
+		txtVoltage.requestFocus();
 	}
 
 }
