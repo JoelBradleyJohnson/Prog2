b96ea71dce152060f9c6defa2a750c690f1629b9
216280 <216280@PLSLAB20751.hardworku.cofo.edu>
2020-02-10 11:51:05 -0600

    Forth Commit

--- Salesman/src/controller/Controller.java
+++ Salesman/src/controller/Controller.java
@@ -3,26 +3,56 @@ package controller;
 import java.time.LocalDateTime;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import model.Circuit;
 import model.Toy;
 
 public class Controller {
+	
+	@FXML
+    private ChoiceBox<String> choiceBox1;
+
+    @FXML
+    private ChoiceBox<String> choiceBox2;
+    
+    @FXML
+	private TextField txtToyID, txtInspector, txtVoltage1, txtVoltage2, txtResistance1, txtResistance2;
+    
+    /**
+     * Like a constructor. Whatever you want in the program on startup.
+     */
+	@FXML
+	void initialize() {
+		// Initialize Choice Box 1.
+		choiceBox1.getItems().add("United States");
+		choiceBox1.getItems().add("Germany");
+		choiceBox1.getItems().add("China");
+		
+		// Initialize Choice Box 2.
+		choiceBox2.getItems().add("United States");
+		choiceBox2.getItems().add("Germany");
+		choiceBox2.getItems().add("China");
+	}
 
     @FXML
     void handleSave(ActionEvent event) {
     	Toy myToy = new Toy();
     	
     	// Set toy-only properties
-    	myToy.setToyID(txtToyID.getText());
+    	myToy.setToyID(Integer.parseInt(txtToyID.getText()));
     	myToy.setInspector(txtInspector.getText());
     	myToy.setInspectionDateTime(LocalDateTime.now());
     	
     	//Set circuit1 properties
-    	myToy.getCircuit1().setVoltage(txtVoltage1.getText());
-    	myToy.getCircuit1().setResistance(txtResistance1.getText());
-    	
+    	myToy.getCircuit1().setVoltage(Double.parseDouble(txtVoltage1.getText()));
+    	myToy.getCircuit1().setResistance(Double.parseDouble(txtResistance1.getText()));
+    	myToy.getCircuit1().setManufactureLocation(choiceBox1.getSelectionModel().getSelectedItem());
+    	    	
     	//Set circuit2 properties
-    	myToy.getCircuit2().setVoltage(txtVoltage2.getText());
+    	myToy.getCircuit2().setVoltage(Double.parseDouble(txtVoltage2.getText()));
+    	myToy.getCircuit2().setResistance(Double.parseDouble(txtResistance2.getText()));
+    	myToy.getCircuit2().setManufactureLocation(choiceBox2.getSelectionModel().getSelectedItem());
     	
     	//Save everything
     	//myToy.save();
@@ -52,7 +82,13 @@ public class Controller {
 
     @FXML
     void handleClear(ActionEvent event) {
-
+    	txtVoltage1.clear();
+    	txtVoltage2.clear();
+    	txtResistance1.clear();
+    	txtResistance2.clear();
+    	txtToyID.clear();
+    	choiceBox1.getSelectionModel().clearSelection();
+    	choiceBox2.getSelectionModel().clearSelection();
     }
    
 }
